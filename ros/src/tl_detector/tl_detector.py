25bb56026c8e0f4c762104a12525cfeea1f32e18
Wen Chen <iceinsouth@gmail.com>
2019-04-14 22:17:16 +0000

    Disable light classification for a while after see red light turned to green light

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -109,6 +109,8 @@ class TLDetector(object):
             self.state_count = 0
             self.state = state
         elif self.state_count >= STATE_COUNT_THRESHOLD:
+            if self.last_state == TrafficLight.RED and self.state == TrafficLight.GREEN:
+                self.image_counter = -100
             self.last_state = self.state
             light_wp = light_wp if state == TrafficLight.RED else -1
             self.last_wp = light_wp
